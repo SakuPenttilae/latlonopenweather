507d328474c9a4ba833c1cdb340b99d0eb9d4739
Saku Penttilä <sakuinenpenttila@gmail.com>
2022-02-27 18:45:45 +0200

    valmis sovellus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState,useEffect } from "react"
 import './App.css';
+import Weather from "./components/Weather"
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [lat,setLat] = useState(0) 
+  const [lon,setLon] = useState(0) 
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude)
+        setLon(position.coords.longitude)
+        setIsLoading(false)
+
+      }, (error) => {
+        alert(error)
+      })
+    } else {
+      alert("sun selain ei kyl nyt tue geosijaintia oon aika varma ehkä? maby, katotaan kohta tai joskus tos ens vuonna ku nyt peukalo vähä kipee katos")
+    }
+  
+    
+  }, [])
+  
+  if (isLoading) {
+    return <div><p>oota ku lattaan</p></div>
+  } else {
+    return (
+      <div>
+      <p>
+        Täälläkö nää nyt oleskelet?: 
+        {lat},
+        {lon}
+      </p>
+      <Weather lat={lat} lon={lon}/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?"
+const ICON_URL = "https://openweathermap.org/img/wn/"
+const API_KEY = "f2d92291c6379fafa209e1e5b16e0636"
+
+export default function Weather({lat, lon}) {
+  const [temp, setTemp] = useState(0)
+  const [speed, setSpeed] = useState(0)
+  const [direction, setDirection] = useState(0)
+  const [description, setDescription] = useState("")
+  const [icon, setIcon] = useState("")
+
+    useEffect(() => {
+      const address = API_URL + 
+        "lat=" + lat +
+        "&lon=" + lon +
+        "&units=metric" +
+        "&appid="+ API_KEY
+        //console.log(address)
+
+        axios.get(address)
+            .then ((response) => {
+            console.log(response.data)
+            setTemp(response.data.main.temp)
+            setSpeed(response.data.wind.speed)
+            setDirection(response.data.wind.deg)
+            setDescription(response.data.weather[0].description)
+            setIcon(ICON_URL +  response.data.weather[0].icon + "@2x.png")
+
+            }).catch (error => {
+                alert(error)
+            })
+        
+    }, [])
+    
+
+  return (
+    <>
+        <h3>Sää sun paikan perusteel:</h3>
+        <p>{temp} C&#176;</p>
+        <p>{speed} m/s {direction} degrees</p>
+        <p>{description}</p>
+        <img src={icon} alt="weather icon"/>
+    </>
+  )
+}
